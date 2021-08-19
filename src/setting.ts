73e25ee6f795ad6aafd0be03206999a1ba21ffc9
renmu <1101022351@qq.com>
2021-08-19 15:51:06 +0800

    add pasted auto upload Switch

--- src/setting.ts
+++ src/setting.ts
@@ -2,10 +2,12 @@ import { App, PluginSettingTab, Setting } from "obsidian";
 import imageAutoUploadPlugin from "./main";
 
 export interface PluginSettings {
+  uploadByClipSwitch: boolean;
   uploadServer: string;
 }
 
 export const DEFAULT_SETTINGS: PluginSettings = {
+  uploadByClipSwitch: true,
   uploadServer: "http://127.0.0.1:36677/upload",
 };
 
@@ -22,6 +24,20 @@ export class SettingTab extends PluginSettingTab {
 
     containerEl.empty();
     containerEl.createEl("h2", { text: "plugin settings" });
+    new Setting(containerEl)
+      .setName("pasted auto upload Switch")
+      .setDesc(
+        "if you set this value true, when you paste image, it will be auto uploaded(you should set the picGo server rightly)"
+      )
+      .addToggle(toggle =>
+        toggle
+          .setValue(this.plugin.settings.uploadByClipSwitch)
+          .onChange(async value => {
+            this.plugin.settings.uploadByClipSwitch = value;
+            await this.plugin.saveSettings();
+          })
+      );
+
     new Setting(containerEl)
       .setName("picGo server")
       .setDesc("picGo server")
