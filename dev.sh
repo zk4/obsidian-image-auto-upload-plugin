53c8e99e07d42123790e4f9b8f00b99a4fa1c349
zk <liuzq7@gmail.com>
2022-12-23 17:28:41 +0800

    chore: u

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ln -fs /Users/zk/git/obsidian_plugin/obsidian-image-auto-upload-plugin/dist '/Users/zk/Library/Mobile Documents/iCloud~md~obsidian/Documents/note/.obsidian/plugins/uploader'
+ln -fs /Users/zk/git/obsidian_plugin/obsidian-image-auto-upload-plugin/dist '/Users/zk/Library/Mobile Documents/iCloud~md~obsidian/Documents/note/.obsidian/plugins/zk-picgo-uploader'
