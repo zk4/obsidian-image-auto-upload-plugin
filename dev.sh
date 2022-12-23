1b7d21014c541054f0b3c0662d9a13d517b2b6fe
zk <liuzq7@gmail.com>
2022-12-23 17:32:01 +0800

    chore: update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+yarn build
 ln -fs /Users/zk/git/obsidian_plugin/obsidian-image-auto-upload-plugin/dist '/Users/zk/Library/Mobile Documents/iCloud~md~obsidian/Documents/note/.obsidian/plugins/zk-picgo-uploader'
